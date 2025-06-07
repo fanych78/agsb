d7eba9d013b7e663577fd9b7729514c9bc3e4a12
fanych78 <870938841@qq.com>
2025-06-07 17:03:37 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
+USER_NAME = "fan2025"         # 用户名
+UUID = "96e26721-f720-417b-ae1b-23c184110ca8"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "idx20250509.fanfirst.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiYjk2OTAwYzlmNmRhYjFkZWM1YWE2ZTlmMzdkZjUzZDciLCJ0IjoiYjYxMDIyZmQtNmI0MS00YjRhLWE3Y2ItNjZjNjJlMzVjYjgxIiwicyI6Ik5XUTJNamRoWW1ZdE9EVTBOeTAwT1RobExUbGxPREF0WTJVek1qSTFOMkprTVdReCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
